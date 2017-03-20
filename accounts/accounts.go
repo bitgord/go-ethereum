7fd603373582e274b5727a689c73ba9d561ca7fd
Michael Gord <michael@mlgblockchain.com>
2017-03-20 13:51:02 -0400

    change import

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -20,10 +20,10 @@ package accounts
 import (
 	"math/big"
 
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
+	ethereum "github.com/Toronto-Coin/go-TorontoCoin"
+	"github.com/Toronto-Coin/go-TorontoCoin/common"
+	"github.com/Toronto-Coin/go-TorontoCoin/core/types"
+	"github.com/Toronto-Coin/go-TorontoCoin/event"
 )
 
 // Account represents an Ethereum account located at a specific location defined
